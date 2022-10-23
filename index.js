64bbcb5011f2e18fa4564f55d597a1fb0f8c6e8e
Paulo Alberto Isidro Rafael Briceno Gonzalez <pabrcn@gmail.com>
2022-10-23 10:32:21 -0300

    idem

--- index.js
+++ index.js
@@ -1,4 +1,7 @@
 require("@babel/register")();
 require("@babel/register")({ extensions: [".js", ".ts"] });
 const app = require("./src/app");
-app.default();
+
+app.default.listen(app.port, () =>
+  console.log(`Server running on port ${app.port}`)
+);
--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,6 @@
 import express from "express";
 
-const app = async () => {
-  const init: string = "Hello World!";
-  const application = express();
-  application.listen(3000, () => console.log(init));
-};
+const app = express();
+export const port = 3000;
 
 export default app;
